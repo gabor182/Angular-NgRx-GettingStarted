b365a11bd24a3a780d541cdaa8b556076181890e
Gabor Schmied <gabor.schmied@cognex.com>
2018-09-10 16:13:33 +0200

    using strongly typed actions to load, edit and create new products

--- APM-Demo0/src/app/products/product-list/product-list.component.ts
+++ APM-Demo0/src/app/products/product-list/product-list.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-
-import { Subscription } from 'rxjs';
-
 import { Product } from '../product';
 import { ProductService } from '../product.service';
 import { Store, select } from '@ngrx/store';
@@ -23,13 +20,13 @@ export class ProductListComponent implements OnInit, OnDestroy {
 
 	// Used to highlight the selected product in the list
 	selectedProduct: Product | null;
-	sub: Subscription;
 
 	constructor(private productService: ProductService, private store: Store<fromProduct.State>) { }
 
 	ngOnInit(): void {
-		this.sub = this.productService.selectedProductChanges$.subscribe(
-			selectedProduct => this.selectedProduct = selectedProduct
+		// TODO: unsubscribe
+		this.store.pipe(select(fromProduct.getCurrentProduct)).subscribe(
+			currentProduct => this.selectedProduct = currentProduct
 		);
 
 		this.productService.getProducts().subscribe(
@@ -37,13 +34,14 @@ export class ProductListComponent implements OnInit, OnDestroy {
 			(err: any) => this.errorMessage = err.error
 		);
 
+		// TODO: unsubscribe
 		this.store.pipe(select(fromProduct.getShowProductCode)).subscribe(
 			(showProductCode) => this.displayCode = showProductCode
 		);
 	}
 
 	ngOnDestroy(): void {
-		this.sub.unsubscribe();
+
 	}
 
 	checkChanged(value: boolean): void {
@@ -51,11 +49,11 @@ export class ProductListComponent implements OnInit, OnDestroy {
 	}
 
 	newProduct(): void {
-		this.productService.changeSelectedProduct(this.productService.newProduct());
+		this.store.dispatch(new productActions.InitializeCurrentProduct());
 	}
 
 	productSelected(product: Product): void {
-		this.productService.changeSelectedProduct(product);
+		this.store.dispatch(new productActions.SetCurrentProduct(product));
 	}
 
 }
